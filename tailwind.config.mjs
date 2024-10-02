df4e4f05e45200bfc86e9e02a005223ab57419de
pom421 <mauguet.po@gmail.com>
2024-10-02 12:35:24 +0200

    feat: styles tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "selector",
 	theme: {
+		fontFamily: {
+			sans: ["Quicksand", "sans-serif"],
+			julius: ["Julius Sans One", "sans-serif"],
+			convergence: ["Convergence", "sans-serif"]
+		},
 		extend: {},
 	},
 	plugins: [
